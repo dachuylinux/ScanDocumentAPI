45a27e802c8de56266a0f97d599aa7215d05d4f2
dachuylinux <dachuylinux@gmail.com>
2019-09-09 11:32:47 +0700

    Update run_program.bat

--- files/run_program.bat
+++ files/run_program.bat
@@ -4,5 +4,5 @@
 SET java_path=C:\Program Files (x86)\Java\jdk1.8.0_221
 SET currentPath=%cd%
 cd %java_path%\bin\
-start java -Dserver.port=9090 -jar "%currentPath%"\scan-document-api-0.0.1-SNAPSHOT.jar
+start java -Dserver.port=8080 -jar "%currentPath%"\scan-document-api-0.0.1-SNAPSHOT.jar
 EXIT
